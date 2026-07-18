727d096c0058edaac11f36f02b454887aec87be9
yeoni <happyksy0501@gmail.com>
2026-07-19 00:41:54 +0900

    feat: 이미지별 전처리, 하나의 코드로 이미지 전체 전처리 시도 코드 작성 및 이미지 생성

--- ai/ocr.py
+++ ai/ocr.py
@@ -2,7 +2,13 @@ import easyocr
 import cv2
 reader = easyocr.Reader(['ko', 'en'])
 
-result = reader.readtext("../doc/easyOCR_testImg/img_010_36px.png")
+# result = reader.readtext("./enhanced_v2/dilate_img_001_3px.png")
+# result = reader.readtext("./enhanced_v2/dilate_img_003_16px.png")
+# result = reader.readtext("./enhanced_v2/erode_img_004_100px.png")
+# result = reader.readtext("./enhanced_v2/erode_img_005_50px.png")
+# result = reader.readtext("./enhanced_v2/dilate_img_006_24px.png")
+# result = reader.readtext("./enhanced_v2/erode_img_008_21px_69L.png")
+result = reader.readtext("./enhanced_v2/erode_img_009_21px_68L_lying_v2.png")
 
 for r in result:
     print("인식 결과 :", r[1])
